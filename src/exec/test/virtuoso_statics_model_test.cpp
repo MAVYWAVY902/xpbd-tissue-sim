3316d8f224eace2ce8cbc0d2ca59d5852aa52d5d
Sam Tobin <smtobin@wisc.edu>
2025-09-04 15:47:27 -0400

    Fixed base moment and force calculation for Virtuoso arm

--- src/exec/test/virtuoso_statics_model_test.cpp
+++ src/exec/test/virtuoso_statics_model_test.cpp
@@ -21,9 +21,9 @@ int main()
 {
     // create VirtuosoArm
     Config::VirtuosoArmConfig config("arm1", 
-        Vec3r(0,0,0), Vec3r(0,0,0), Vec3r(0,0,0), false, false,
+        Vec3r(10,10,10), Vec3r(0,30,0), Vec3r(0,0,0), false, false,
         1.56e-3, 1.14e-3, 1.5383e-2, 5e-3, 1.04e-3, 0.82e-3,
-        0, 10e-3, 0, 20e-3,
+        0, 4e-3, 0, 20e-3,
         Sim::VirtuosoArm::ToolType::SPATULA, Config::ObjectRenderConfig());
 
     std::unique_ptr<Sim::VirtuosoArm> arm = config.createObject(nullptr);
@@ -37,7 +37,7 @@ int main()
     std::cout << "Outer tube tip position: " << ot_frames.back().origin()[0] << ", " << ot_frames.back().origin()[1] << ", " << ot_frames.back().origin()[2] << std::endl;
 
     // arm->setOuterTubeNodalForce(4, Vec3r(0,-10,0));
-    arm->setInnerTubeNodalForce(8, Vec3r(0,10,0));
+    arm->setInnerTubeNodalForce(9, Vec3r(0,10,0));
     // arm->setTipForce(Vec3r(0,50,0));
     arm->update();
 
@@ -53,74 +53,74 @@ int main()
 
     vtkNew<vtkOpenGLRenderer> renderer;
     renderer->SetBackground(1.0, 1.0, 1.0);
-    arm_graphics_obj.actor()->GetProperty()->SetOpacity(0.2);
+    // arm_graphics_obj.actor()->GetProperty()->SetOpacity(0.2);
     renderer->AddActor(arm_graphics_obj.actor());
 
     // draw collision geometry
-    for (unsigned i = 0; i < ot_frames.size()-1; i++)
-    {
-        Vec3r start = ot_frames[i].origin();
-        Vec3r end = ot_frames[i+1].origin();
-        vtkNew<vtkCylinderSource> cyl_source;
-        cyl_source->SetHeight( (end-start).norm() );
-        cyl_source->SetRadius( arm->outerTubeOuterDiameter()/2.0 );
-        cyl_source->SetResolution(20);
-        cyl_source->CapsuleCapOn();
+    // for (unsigned i = 0; i < ot_frames.size()-1; i++)
+    // {
+    //     Vec3r start = ot_frames[i].origin();
+    //     Vec3r end = ot_frames[i+1].origin();
+    //     vtkNew<vtkCylinderSource> cyl_source;
+    //     cyl_source->SetHeight( (end-start).norm() );
+    //     cyl_source->SetRadius( arm->outerTubeOuterDiameter()/2.0 );
+    //     cyl_source->SetResolution(20);
+    //     cyl_source->CapsuleCapOn();
 
-        vtkNew<vtkPolyDataMapper> mapper;
-        mapper->SetInputConnection(cyl_source->GetOutputPort());
+    //     vtkNew<vtkPolyDataMapper> mapper;
+    //     mapper->SetInputConnection(cyl_source->GetOutputPort());
 
-        vtkNew<vtkActor> actor;
-        actor->SetMapper(mapper);
-        actor->GetProperty()->SetColor(1.0, 1.0, 0.0);
-        actor->GetProperty()->SetOpacity(0.4);
-        renderer->AddActor(actor);
+    //     vtkNew<vtkActor> actor;
+    //     actor->SetMapper(mapper);
+    //     actor->GetProperty()->SetColor(1.0, 1.0, 0.0);
+    //     actor->GetProperty()->SetOpacity(0.4);
+    //     renderer->AddActor(actor);
 
-        // IMPORTANT: use row-major ordering since that is what VTKTransform expects (default for Eigen is col-major)
-        Eigen::Matrix<Real, 4, 4, Eigen::RowMajor> cyl_transform_mat = ot_frames[i].transform().asMatrix();
-        cyl_transform_mat.block<3,1>(0,3) = (start+end)/2.0;
-        vtkNew<vtkTransform> vtk_transform;
-        vtk_transform->SetMatrix(cyl_transform_mat.data());
+    //     // IMPORTANT: use row-major ordering since that is what VTKTransform expects (default for Eigen is col-major)
+    //     Eigen::Matrix<Real, 4, 4, Eigen::RowMajor> cyl_transform_mat = ot_frames[i].transform().asMatrix();
+    //     cyl_transform_mat.block<3,1>(0,3) = (start+end)/2.0;
+    //     vtkNew<vtkTransform> vtk_transform;
+    //     vtk_transform->SetMatrix(cyl_transform_mat.data());
 
-        // vtkCylinderSource creates a cylinder along the y-axis, but we expect the cylinder to be along the z-axis
-        // hence we need to first rotate the cylinder provided by vtkCylinderSource by -90 deg about the x-axis
-        vtk_transform->PreMultiply();
-        vtk_transform->RotateX(-90);
+    //     // vtkCylinderSource creates a cylinder along the y-axis, but we expect the cylinder to be along the z-axis
+    //     // hence we need to first rotate the cylinder provided by vtkCylinderSource by -90 deg about the x-axis
+    //     vtk_transform->PreMultiply();
+    //     vtk_transform->RotateX(-90);
 
-        actor->SetUserTransform(vtk_transform);
-    }
-    for (unsigned i = 0; i < it_frames.size()-1; i++)
-    {
-        Vec3r start = it_frames[i].origin();
-        Vec3r end = it_frames[i+1].origin();
-        vtkNew<vtkCylinderSource> cyl_source;
-        cyl_source->SetHeight( (end-start).norm() );
-        cyl_source->SetRadius( arm->innerTubeOuterDiameter()/2.0 );
-        cyl_source->SetResolution(20);
-        cyl_source->CapsuleCapOn();
+    //     actor->SetUserTransform(vtk_transform);
+    // }
+    // for (unsigned i = 0; i < it_frames.size()-1; i++)
+    // {
+    //     Vec3r start = it_frames[i].origin();
+    //     Vec3r end = it_frames[i+1].origin();
+    //     vtkNew<vtkCylinderSource> cyl_source;
+    //     cyl_source->SetHeight( (end-start).norm() );
+    //     cyl_source->SetRadius( arm->innerTubeOuterDiameter()/2.0 );
+    //     cyl_source->SetResolution(20);
+    //     cyl_source->CapsuleCapOn();
 
-        vtkNew<vtkPolyDataMapper> mapper;
-        mapper->SetInputConnection(cyl_source->GetOutputPort());
+    //     vtkNew<vtkPolyDataMapper> mapper;
+    //     mapper->SetInputConnection(cyl_source->GetOutputPort());
 
-        vtkNew<vtkActor> actor;
-        actor->SetMapper(mapper);
-        actor->GetProperty()->SetColor(1.0, 1.0, 0.0);
-        actor->GetProperty()->SetOpacity(0.4);
-        renderer->AddActor(actor);
+    //     vtkNew<vtkActor> actor;
+    //     actor->SetMapper(mapper);
+    //     actor->GetProperty()->SetColor(1.0, 1.0, 0.0);
+    //     actor->GetProperty()->SetOpacity(0.4);
+    //     renderer->AddActor(actor);
 
-        // IMPORTANT: use row-major ordering since that is what VTKTransform expects (default for Eigen is col-major)
-        Eigen::Matrix<Real, 4, 4, Eigen::RowMajor> cyl_transform_mat = it_frames[i].transform().asMatrix();
-        cyl_transform_mat.block<3,1>(0,3) = (start+end)/2.0;
-        vtkNew<vtkTransform> vtk_transform;
-        vtk_transform->SetMatrix(cyl_transform_mat.data());
+    //     // IMPORTANT: use row-major ordering since that is what VTKTransform expects (default for Eigen is col-major)
+    //     Eigen::Matrix<Real, 4, 4, Eigen::RowMajor> cyl_transform_mat = it_frames[i].transform().asMatrix();
+    //     cyl_transform_mat.block<3,1>(0,3) = (start+end)/2.0;
+    //     vtkNew<vtkTransform> vtk_transform;
+    //     vtk_transform->SetMatrix(cyl_transform_mat.data());
 
-        // vtkCylinderSource creates a cylinder along the y-axis, but we expect the cylinder to be along the z-axis
-        // hence we need to first rotate the cylinder provided by vtkCylinderSource by -90 deg about the x-axis
-        vtk_transform->PreMultiply();
-        vtk_transform->RotateX(-90);
+    //     // vtkCylinderSource creates a cylinder along the y-axis, but we expect the cylinder to be along the z-axis
+    //     // hence we need to first rotate the cylinder provided by vtkCylinderSource by -90 deg about the x-axis
+    //     vtk_transform->PreMultiply();
+    //     vtk_transform->RotateX(-90);
 
-        actor->SetUserTransform(vtk_transform);
-    }
+    //     actor->SetUserTransform(vtk_transform);
+    // }
 
     
 
--- src/simobject/VirtuosoArm.cpp
+++ src/simobject/VirtuosoArm.cpp
@@ -136,8 +136,8 @@ void VirtuosoArm::setup()
 
 void VirtuosoArm::update()
 {
-    const Geometry::TransformationMatrix T_tip = _computeTipTransform(_ot_rotation, _ot_translation, _it_rotation, _it_translation);
-    _hybridDifferentialInverseKinematics(_commanded_tip_position - T_tip.translation());
+    // const Geometry::TransformationMatrix T_tip = _computeTipTransform(_ot_rotation, _ot_translation, _it_rotation, _it_translation);
+    // _hybridDifferentialInverseKinematics(_commanded_tip_position - T_tip.translation());
     if (_stale_frames)
     {
         // _recomputeCoordinateFrames();
@@ -157,25 +157,33 @@ void VirtuosoArm::velocityUpdate()
     std::vector<Vec3r> new_forces(NUM_OT_FRAMES + NUM_IT_FRAMES, Vec3r::Zero());
     for (const auto& collision : _collision_constraints)
     {
+        // the collision constraints give forces on the tissue, so we must negate them to get the forces on the Virtuoso
         std::vector<Vec3r> forces = collision.proj_ref.constraintForces();
-        Vec3r net_force = std::reduce(forces.cbegin(), forces.cend());
+        Vec3r net_force = -std::reduce(forces.cbegin(), forces.cend());  
         new_forces[collision.node_index] += net_force*(1-collision.interp);
         new_forces[collision.node_index+1] += net_force*collision.interp;
     }
 
+    Vec3r total_force = Vec3r::Zero();
     const Real frac = 0.01;
     for (int i = 0; i < NUM_OT_FRAMES; i++)
     {
         const Vec3r& cur_force = outerTubeNodalForce(i);
         Vec3r new_force = (1-frac)*cur_force + frac*new_forces[i];
         setOuterTubeNodalForce(i, new_force);
+
+        total_force += new_force;
     }
     for (int i = 0; i < NUM_IT_FRAMES; i++)
     {
         const Vec3r& cur_force = innerTubeNodalForce(i);
         Vec3r new_force = (1-frac)*cur_force + frac*new_forces[NUM_OT_FRAMES + i];
         setInnerTubeNodalForce(i, new_force);
+
+        total_force += new_force;
     }
+
+    std::cout << "Total force on CTR: " << total_force.transpose() << std::endl;
     _stale_frames = true;
 }
 
@@ -191,7 +199,7 @@ Real VirtuosoArm::_outerTubeClearanceAngle(Real ot_trans)
 
     Real sqrt_arg = tubed_length*tubed_length + 2*OT_ENDOSCOPE_CLEARANCE*OT_RADIUS_OF_CURVATURE - OT_ENDOSCOPE_CLEARANCE*OT_ENDOSCOPE_CLEARANCE;
     Real atan_arg = (tubed_length - std::sqrt(sqrt_arg)) / (OT_ENDOSCOPE_CLEARANCE - 2*OT_RADIUS_OF_CURVATURE);
-    return 2*std::atan(atan_arg);
+    return 0;//2*std::atan(atan_arg);
 }
 
 void VirtuosoArm::_toolAction()
@@ -329,149 +337,6 @@ void VirtuosoArm::_recomputeCoordinateFrames()
 
 }
 
-void VirtuosoArm::_recomputeCoordinateFramesStaticsModel()
-{
-    // compute arm base frame based on current position and orientation
-    _arm_base_frame = Geometry::CoordinateFrame(Geometry::TransformationMatrix(_arm_base_rotation, _arm_base_position));
-
-    // compute outer tube base frame
-    // consists of rotating about the current z-axis accordint to outer tube rotation
-    Geometry::TransformationMatrix T_rot_z(GeometryUtils::Rz(_ot_rotation), Vec3r::Zero());
-    Geometry::CoordinateFrame ot_base_frame = _arm_base_frame * T_rot_z;
-    
-    // calculate internal forces and moments carried at the base of the tube collection
-    const Vec3r& tip_position = innerTubeEndFrame().origin();
-    // add contributions from force and moment at tube tip
-    Vec3r base_moment = _tip_moment + tip_position.cross(_tip_force);
-    Vec3r base_force = _tip_force;
-
-
-    // EVERYTHING FROM NOW ON WILL USE MM
-    // compute cross section properties
-    Real E = 60e9/1000/1000;    // Young's Modulus (N/mm^2)
-    Real G = E / (2*(1+0.3));   // Shear modulus (N/mm^2)
-
-    Real ot_outer_dia_mm = _ot_outer_dia * 1000;
-    Real ot_inner_dia_mm = _ot_inner_dia * 1000;
-    Real it_outer_dia_mm = _it_outer_dia * 1000;
-    Real it_inner_dia_mm = _it_inner_dia * 1000;
-    Real I_ot = M_PI/4 * (ot_outer_dia_mm*ot_outer_dia_mm*ot_outer_dia_mm*ot_outer_dia_mm/16 - ot_inner_dia_mm*ot_inner_dia_mm*ot_inner_dia_mm*ot_inner_dia_mm/16);
-    Real I_it = M_PI/4 * (it_outer_dia_mm*it_outer_dia_mm*it_outer_dia_mm*it_outer_dia_mm/16 - it_inner_dia_mm*it_inner_dia_mm*it_inner_dia_mm*it_inner_dia_mm/16);
-    Real J_ot = 2*I_ot;
-    Real J_it = 2*I_it;
-
-    // integrate the curved section of the outer tube
-    Real curved_length = (_ot_translation - _ot_distal_straight_length)*1000; // in mm
-
-    TubeIntegrationState ot_base_state;
-    ot_base_state.position = ot_base_frame.origin()*1000;   // put position in mm
-    ot_base_state.orientation = ot_base_frame.transform().rotMat();
-    ot_base_state.internal_force = base_force;
-    ot_base_state.internal_moment = base_moment*1000; // put it in N-mm
-    ot_base_state.torsional_displacement = _ot_rotation;
-
-    TubeIntegrationState ot_curve_end_state;
-    if (curved_length > 0)
-    {
-        // std::cout << "Outer tube curved section length > 0 (=" << curved_length << " mm)" << std::endl;
-        const Vec3r K_inv(1/(E*I_ot + E*I_it), 1/(E*I_ot + E*I_it), 1/(G*J_ot));
-        const Vec3r precurvature(1/_ot_r_curvature/1000, 0, 0);
-
-        std::vector<Real> ot_curve_s(NUM_OT_CURVE_FRAMES);  // distance along tube for outer curve frames (in mm)
-        for (int i = 0; i < NUM_OT_CURVE_FRAMES; i++)
-            ot_curve_s[i] = i* (curved_length / (NUM_OT_CURVE_FRAMES-1));
-
-        std::vector<TubeIntegrationState::VecType> ot_curve_states = _integrateTubeRK4(ot_base_state, ot_curve_s, K_inv, precurvature);
-        
-        for (int i = 0; i < NUM_OT_CURVE_FRAMES; i++)
-        {
-            TubeIntegrationState state = TubeIntegrationState::fromVec(ot_curve_states[i]);
-            _ot_frames[i].setTransform(Geometry::TransformationMatrix(state.orientation, state.position/1000).asMatrix()); // convert position back to m
-        }
-
-        ot_curve_end_state = TubeIntegrationState::fromVec(ot_curve_states.back());
-    }
-    else
-    {
-        for (int i = 0; i < NUM_OT_CURVE_FRAMES; i++)
-        {
-            _ot_frames[i].setTransform(ot_base_frame.transform().asMatrix());
-        }
-        ot_curve_end_state = ot_base_state;
-    }
-
-    // integrate straight section of outer tube
-    Real straight_length = std::min(_ot_translation, _ot_distal_straight_length)*1000; // length of outer tube straight section (in mm)
-    TubeIntegrationState ot_end_state;
-    if (straight_length > 0)
-    {
-        // std::cout << "Outer tube straight section length > 0 mm (=" << straight_length << " mm)" << std::endl;
-        const Vec3r ot_K_inv(1/(E*I_ot + E*I_it), 1/(E*I_ot + E*I_it), 1/(G*J_ot));
-        std::vector<Real> ot_straight_s(NUM_OT_STRAIGHT_FRAMES+1);
-        for (int i = 0; i < NUM_OT_STRAIGHT_FRAMES+1; i++)
-            ot_straight_s[i] = curved_length + i*straight_length / NUM_OT_STRAIGHT_FRAMES;
-        std::vector<TubeIntegrationState::VecType> ot_straight_states = _integrateTubeRK4(ot_curve_end_state, ot_straight_s, ot_K_inv, Vec3r(0,0,0));
-        
-
-        for (int i = 0; i < NUM_OT_STRAIGHT_FRAMES; i++)
-        {
-            TubeIntegrationState state = TubeIntegrationState::fromVec(ot_straight_states[i+1]);
-            _ot_frames[NUM_OT_CURVE_FRAMES + i].setTransform(Geometry::TransformationMatrix(state.orientation, state.position/1000).asMatrix()); // convert position back to m
-        }
-
-        ot_end_state = TubeIntegrationState::fromVec(ot_straight_states.back());
-    }
-    else
-    {
-        for (int i = 0; i < NUM_OT_STRAIGHT_FRAMES; i++)
-        {
-            _ot_frames[NUM_OT_CURVE_FRAMES + i].setTransform(_ot_frames[NUM_OT_CURVE_FRAMES-1].transform().asMatrix());
-        }
-        
-        ot_end_state = ot_curve_end_state;
-    }
-
-    // integrate inner tube
-    const Real it_length = std::max(Real(0.0), _it_translation - _ot_translation) * 1000;      // exposed length of the inner tube in mm
-    TubeIntegrationState it_start_state = ot_end_state;
-    it_start_state.orientation *= GeometryUtils::Rz(_it_rotation - it_start_state.torsional_displacement);
-    it_start_state.torsional_displacement = _it_rotation;
-
-    if (it_length > 0)
-    {
-        // std::cout << "Inner tube length > 0... (=" << it_length << " mm)" << std::endl;
-        
-        const Vec3r it_K_inv(1/(E*I_it), 1/(E*I_it), 1/(G*J_it));
-        std::vector<Real> it_s(NUM_IT_FRAMES);
-        for (int i = 0; i < NUM_IT_FRAMES; i++)     
-        {
-            it_s[i] = _ot_translation*1000 + i*it_length / (NUM_IT_FRAMES - 1);
-        }
-        
-        std::vector<TubeIntegrationState::VecType> it_states = _integrateTubeRK4(it_start_state, it_s, it_K_inv, Vec3r(0,0,0));
-
-        for (int i = 0; i < NUM_IT_FRAMES; i++)
-        {
-            TubeIntegrationState state = TubeIntegrationState::fromVec(it_states[i]);
-            _it_frames[i].setTransform(Geometry::TransformationMatrix(state.orientation, state.position/1000).asMatrix()); // convert position back to m
-        }
-
-    }
-    else
-    {
-        Geometry::TransformationMatrix it_start_transform(it_start_state.orientation, it_start_state.position/1000); // convert position back to m
-        for (int i = 0; i < NUM_IT_FRAMES; i++)
-        {
-            _it_frames[i].setTransform(it_start_transform.asMatrix());
-        }
-    }
-    
-    _stale_frames = false;
-
-    // for now, the tool position is just the inner tube tip position with no offset
-    _tool_position = _it_frames.back().origin();
-}
-
 void VirtuosoArm::_recomputeCoordinateFramesStaticsModelWithNodalForces()
 {
     // compute arm base frame based on current position and orientation
@@ -487,18 +352,20 @@ void VirtuosoArm::_recomputeCoordinateFramesStaticsModelWithNodalForces()
     // calculate internal forces and moments carried at the base of the tube collection
     const Vec3r& tip_position = innerTubeEndFrame().origin();
     // add contributions from force and moment at tube tip
-    Vec3r base_moment = _tip_moment + tip_position.cross(_tip_force);
-    Vec3r base_force = _tip_force;
+    Vec3r base_moment = _tip_moment + (tip_position - _arm_base_frame.origin()).cross(_tip_force);
+    Vec3r base_force = -_tip_force;
     // add contributions from forces applied along the tube
     for (int i = 0; i < NUM_OT_FRAMES; i++)
     {
-        base_moment += _ot_frames[i].origin().cross(_ot_nodal_forces[i]);
-        base_force += _ot_nodal_forces[i];
+        Vec3r moment_arm = _ot_frames[i].origin() - _arm_base_frame.origin();
+        base_moment += moment_arm.cross(_ot_nodal_forces[i]);
+        base_force += -_ot_nodal_forces[i];
     }
     for (int i = 0; i < NUM_IT_FRAMES; i++)
     {
-        base_moment += _it_frames[i].origin().cross(_it_nodal_forces[i]);
-        base_force += _it_nodal_forces[i];
+        Vec3r moment_arm = _it_frames[i].origin() - _arm_base_frame.origin();
+        base_moment += moment_arm.cross(_it_nodal_forces[i]);
+        base_force += -_it_nodal_forces[i];
     }
 
     // EVERYTHING FROM NOW ON WILL USE MM
@@ -922,7 +789,7 @@ Eigen::Matrix<Real,6,3> VirtuosoArm::_3DOFSpatialJacobian()
     Real tubed_length = std::max(_ot_distal_straight_length - _ot_translation, Real(0.0));
     Real sqrt_arg = tubed_length*tubed_length + 2*OT_ENDOSCOPE_CLEARANCE*OT_RADIUS_OF_CURVATURE - OT_ENDOSCOPE_CLEARANCE*OT_ENDOSCOPE_CLEARANCE;
     Real atan_arg = (tubed_length - std::sqrt(sqrt_arg)) / (OT_ENDOSCOPE_CLEARANCE - 2*OT_RADIUS_OF_CURVATURE);
-    Real dca_d1 = 2 / (1+atan_arg*atan_arg) / (OT_ENDOSCOPE_CLEARANCE - 2*OT_RADIUS_OF_CURVATURE) * (1 - tubed_length/std::sqrt(sqrt_arg)) * dtubed_length_d1;
+    Real dca_d1 = 0;//2 / (1+atan_arg*atan_arg) / (OT_ENDOSCOPE_CLEARANCE - 2*OT_RADIUS_OF_CURVATURE) * (1 - tubed_length/std::sqrt(sqrt_arg)) * dtubed_length_d1;
     
     Real clearance_angle = _outerTubeClearanceAngle(_ot_translation);
     const Real alpha = std::max(_ot_translation - _ot_distal_straight_length, Real(0.0)) / _ot_r_curvature;    // angle swept by the outer tube curve
